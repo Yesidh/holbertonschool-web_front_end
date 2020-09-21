80d6311d4c257d15249dfa2b4a67a30a5eed21a1
Yesidh <944@holbertonschool.com>
2020-09-21 16:19:51 -0500

    Clousure and loops

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,13 @@
+function createClassRoom(numbersOfStudents) {
+    function studentSeat (seat) {   
+        return  function () { 
+            return seat; 
+        };
+    };
+    let students = new Array(numbersOfStudents);
+    for (let i = 0; i < numbersOfStudents; i++) {
+        students[i] = studentSeat(i + 1);
+    }
+    return students;
+}
+const classRoom = createClassRoom(10) 
