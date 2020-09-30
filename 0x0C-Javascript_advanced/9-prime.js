0a5d5293b8f84ae1d29cf7de563479f8262ccef4
Yesidh <944@holbertonschool.com>
2020-09-29 23:30:12 -0500

    Prime numbers & timing execution.

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,14 @@
+function countPrimeNumbers() {
+    let countPrimeNum = 97;
+    for(let i = 3; i<= 100; i++ ){
+        if ((i % 2 == 0) || (i % 3 == 0) || (i % 5 == 0)){
+            countPrimeNum --;
+        }
+    }
+    return countPrimeNum
+}
+
+let firstTimePerformance = performance.now();
+countPrimeNumbers();
+let secondTimePerformance = performance.now();
+console.log(`Execution time of printing countPrimeNumbers was ${ secondTimePerformance - firstTimePerformance} milliseconds.`);
